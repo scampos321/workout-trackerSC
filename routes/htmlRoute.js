f2592e3991700afd0992ec1c72e5fbb2f0fec448
scampos321 <sebastian.campos321@gmail.com>
2021-11-04 21:00:35 -0400

    added styling

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -5,9 +5,9 @@ router.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 router.get("/exercise", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/workout/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 router.get("/stats", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/workout/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
 module.exports = router;
