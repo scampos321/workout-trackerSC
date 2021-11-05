fc6f7c40f45bec6c3ee43109eeccd0c50f12cbb8
scampos321 <sebastian.campos321@gmail.com>
2021-11-04 21:53:25 -0400

    post request for workout

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({body}, res) => {
+    Workout.create(body)
+    .then(workoutDB => {
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+
 module.exports = router;
