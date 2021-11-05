5ed03bbd229da715cc69ced9ebfac406e23c7b0d
scampos321 <sebastian.campos321@gmail.com>
2021-11-04 22:35:42 -0400

    add post request for inserting many into db

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workout", ({body}, res) => {
+router.post("/api/workout", ({ body }, res) => {
     Workout.create(body)
+    .then(workoutDB => {
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/api/workout", ({ body }, res) => {
+    Workout.insertMany(body)
     .then(workoutDB => {
         res.json(workoutDB);
     })
