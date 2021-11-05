aabcf55c48a9ca34c7ce99e15057c4302f43ca31
scampos321 <sebastian.campos321@gmail.com>
2021-11-05 00:21:52 -0400

    changed mongoose version to 5

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ app.use(require("./routes/apiRoute.js"))
 app.use(require("./routes/htmlRoute.js"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 app.use(express.static("public"));
