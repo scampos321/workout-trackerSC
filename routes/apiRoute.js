c454df16892f34796fb639f6d45b3e44c81c52e5
scampos321 <sebastian.campos321@gmail.com>
2021-11-04 22:44:57 -0400

    total duration get routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -21,6 +21,38 @@ router.post("/api/workout", ({ body }, res) => {
     });
 });
 
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+    { $addField: {
+        totalDuration: {
+            $sum: '$exercises.duration'
+        }
+        }}
+    ])
+    .then(workoutDB => {
+    res.json(workoutDB)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        { $addField: {
+            totalDuration: {
+                $sum: '$exercises.duration'
+            }
+        }}
+    ]).sort( {
+        _id: -1
+    }).limit(10)
+    .then(workoutDB => {
+        res.json(workoutDB);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
 
 module.exports = router;
