7ae59bae348633ba3a590a43a1aa000f9ee18ed5
Sebastian Campos <sebastian.campos321@gmail.com>
2021-11-09 23:01:49 -0500

    fix routes

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 app.use(require("./routes/apiRoute.js"))
 app.use(require("./routes/htmlRoute.js"))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
-
-app.use(express.static("public"));
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}!`);
 })
